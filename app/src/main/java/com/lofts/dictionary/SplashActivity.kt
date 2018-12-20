bc80a78b72c599de7b27f55b757f0e26354ba8b8
lofts <gb_2312@126.com>
2018-12-20 15:12:47 +0800

    modify error'
    q
    exit
    q

--- app/src/main/java/com/lofts/dictionary/SplashActivity.kt
+++ app/src/main/java/com/lofts/dictionary/SplashActivity.kt
@@ -26,6 +26,7 @@ class SplashActivity : AppCompatActivity() {
     private fun gotoIndexPage() {
         Handler().postDelayed({
             startActivity<InputActivity>()
+            finish()
         }, delayTime)
     }
 
